17e18aceec3b9af9b2fafdff2c144758d4178cfe
Jerry-hsiao <jerryhsiao790814@gmail.com>
2020-08-27 22:45:18 +0800

    docker compose fix problem

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App GO GO GO 222333!"/>
+    <HelloWorld msg="Welcome to Your Vue.js App GO GO GO! 1 1 1"/>
   </div>
 </template>
 
